1335d81825d5b6e617badbca17c8d42094faed2e
purnale <bhaktipurnale89@gmail.com>
2024-04-26 18:55:01 +0530

    react-portfolio

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export{
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+
+};
